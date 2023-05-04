6bb2ac2dd5b8c5cda8d0df47a88cc4c18115691e
sbk0716 <uhey0716@gmail.com>
2023-05-05 05:26:38 +0900

    add comment

--- src/main/kotlin/big/Application.kt
+++ src/main/kotlin/big/Application.kt
@@ -4,11 +4,18 @@ import io.ktor.server.application.*
 import big.plugins.*
 import my_kotlin_app.DatabaseFactory
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+/**
+ * Declares main function.
+ * This is because the entry point of the application is set to this main function.
+ */
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+/**
+ * In the Application class of io.ktor.server.application,
+ * declare an extension function named `mainModule`.
+ */
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-fun Application.module() {
+fun Application.mainModule() {
     configureSerialization()
     configureSecurity()
     DatabaseFactory.init()
